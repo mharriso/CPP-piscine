188e43054cb1d1d62e8c1148922a82320265c959
mharriso <mharriso@student.21-school.ru>
2021-06-26 18:53:54 +0300

    ex000

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char const **argv)
+{
+	if(argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (size_t i = 1; i < argc; i++)
+		for (size_t j = 0; argv[i][j]; j++)
+			std::cout << (char)toupper(argv[i][j]);
+	std::cout << std::endl;
+	return 0;
+}
+
