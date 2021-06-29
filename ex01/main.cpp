39fd0cd36ae5066cbd2f1f617b44ce76fbd71298
mharriso <mharriso@student.21-school.ru>
2021-06-29 04:58:11 +0300

    format tab

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,6 +3,8 @@
 #include "Contact.hpp"
 
 #define NUM_CONTACTS 8
+#define GREEN   "\033[32m"
+#define RESET   "\033[0m"
 #define CRAPPY "\U0001F47B"
 
 //first name, last name, nickname, phone number,darkest secret
@@ -33,16 +35,19 @@ void	add_contact(Contact *book)
 	set_input(book[i], "Darkest secret: ", &Contact::setDarkestSecret);
 	i++;
 }
+std::string	form_str(std::string str)
+{
+	if(str.length() > 10)
+		return (str.substr(0, 9) + ".");
+	return (str);
+}
 
 void	print_format(std::string s0, std::string s1, std::string s2, std::string s3)
 {
-	std::cout << std::setw(10) << s0;
-	std::cout << '|';
-	std::cout << std::setw(10) << s1;
-	std::cout << '|';
-	std::cout << std::setw(10) << s2;
-	std::cout << '|';
-	std::cout << std::setw(10) << s3 << std::endl;
+	std::cout << std::setw(10) << form_str(s0) << '|';
+	std::cout << std::setw(10) << form_str(s1) << '|';
+	std::cout << std::setw(10) << form_str(s2) << '|';
+	std::cout << std::setw(10) << form_str(s3) << std::endl;
 }
 
 void	print_book(Contact const *book)
@@ -63,7 +68,7 @@ int	main(int argc, char const *argv[])
 	Contact		book[NUM_CONTACTS];
 	std::string	input;
 
-	std::cout << "crappybook "CRAPPY"> ";
+	std::cout << GREEN"crappybook "CRAPPY"> "RESET;
 	while(getline(std::cin, input))
 	{
 		if(input == "ADD")
@@ -72,7 +77,7 @@ int	main(int argc, char const *argv[])
 			search_contact(book);
 		else if(input == "EXIT")
 			break ;
-		std::cout << "crappybook "CRAPPY"> ";
+		std::cout << GREEN"crappybook "CRAPPY"> "RESET;
 	}
 	return 0;
 }
