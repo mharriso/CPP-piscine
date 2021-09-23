e713c0e7fec39bd5ab41ccf6f36c3ba1909f85fe
mharriso <mharriso@student.21-school.ru>
2021-09-23 21:06:19 +0300

    create tests

--- module_07/ex02/src/main.cpp
+++ module_07/ex02/src/main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 #include "Array.hpp"
+#include "tools.hpp"
 
-#define MAX_VAL 750
-int main(int, char**)
+#define MAX_VAL 221
+
+int		test_int()
 {
     Array<int> numbers(MAX_VAL);
     int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
+	putString("<<<<<<<<< INIT INT ARRAY >>>>>>>>>", C_BLUE);
     for (int i = 0; i < MAX_VAL; i++)
     {
         const int value = rand();
         numbers[i] = value;
         mirror[i] = value;
+		std::cout << value << ' ';
     }
+	std::cout << std::endl;
     //SCOPE
     {
         Array<int> tmp = numbers;
@@ -27,6 +31,7 @@ int main(int, char**)
             return 1;
         }
     }
+	putString("<< Attempt to access an element with a invalid index >>", C_BLUE);
     try
     {
         numbers[-2] = 0;
@@ -49,5 +54,65 @@ int main(int, char**)
         numbers[i] = rand();
     }
     delete [] mirror;//
-    return 0;
+	return 0;
+}
+
+int		test_char()
+{
+	Array<char> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+	putString("<<<<<<<<< INIT CHAR ARRAY >>>>>>>>>", C_BLUE);
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const char value = static_cast<char>(33 + rand() % 94);
+        numbers[i] = value;
+        mirror[i] = value;
+		std::cout << value << ' ';
+    }
+	std::cout << std::endl;
+    //SCOPE
+    {
+        Array<char> tmp = numbers;
+        Array<char> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+	putString("<< Attempt to access an element with a invalid index >>", C_BLUE);
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+	return 0;
+}
+
+int main(int, char**)
+{
+	srand(time(NULL));
+	test_int();
+	test_char();
 }
